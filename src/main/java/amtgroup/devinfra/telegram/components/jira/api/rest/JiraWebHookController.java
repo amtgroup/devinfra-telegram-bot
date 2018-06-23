94940ffc3538a79ae66ef3d8bea08665a20ef7bc
Vitaly Ogoltsov <vitaly.ogoltsov@me.com>
2018-06-23 12:13:59 +0300

    feat: Реализован прием и форматирование уведомлений.

--- src/main/java/amtgroup/devinfra/telegram/components/jira/api/rest/JiraWebHookController.java
+++ src/main/java/amtgroup/devinfra/telegram/components/jira/api/rest/JiraWebHookController.java
@@ -1,9 +1,9 @@
 package amtgroup.devinfra.telegram.components.jira.api.rest;
 
-import amtgroup.devinfra.telegram.components.jira.api.rest.dto.JiraIssueEventRequest;
-import amtgroup.devinfra.telegram.util.exception.NotImplementedException;
+import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,12 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/web-hooks/jira", produces = {MediaType.APPLICATION_JSON_VALUE, "application/hal+json"})
 @Api(description = "Jira Web Hooks", tags = "jira-web-hooks")
+@Slf4j
 public class JiraWebHookController {
 
-    @ApiOperation("")
-    @PostMapping("/{projectKey}/{issueKey}")
-    public void postIssueEvent(@RequestBody JiraIssueEventRequest request) {
-        throw new NotImplementedException();
+    @ApiOperation("WebHook-уведомления от JIRA")
+    @PostMapping("/**")
+    public void register(@RequestBody JsonNode event) {
+        log.warn("Получено уведомление от JIRA: {}", event);
     }
 
 }
